506b904b9003592d1bcb124b808e5af89ee1296c
dougstorm <dougstorm@users.noreply.github.com>
2021-02-11 18:14:28 -0600

    fix: fix failing lint

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -42,10 +42,8 @@ def test_sweep_from_rando(
     gov,
     accounts,
 ):
-    # Registry
+    # Registry v2
     registry = "0xe15461b18ee31b7379019dc523231c57d1cbc18c"
-    ychad = accounts.at(web3.ens.resolve("ychad.eth"), force=True)
-
     token = Token.at("0x6B175474E89094C44Da98b954EedeAC495271d0F")  # DAI
     tokenOwner = accounts.at(
         "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643", force=True
