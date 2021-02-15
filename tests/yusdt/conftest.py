c49e62f78fc4afca1a937241dd136577c010164e
steffel <2143646+steffenix@users.noreply.github.com>
2021-02-15 18:38:33 +0100

    feat: add yusdt zap

---
+++ tests/yusdt/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from brownie import config
+from brownie import Contract
+
+
+@pytest.fixture
+def zapper(accounts, ZapYUsdt):
+    zapper = accounts[0].deploy(ZapYUsdt)
+    yield zapper
+
+
+@pytest.fixture
+def vault(zapper):
+    vault = Contract(zapper.yVault())
+    yield vault
+
+
+@pytest.fixture
+def gov(accounts, vault):
+    gov = vault.governance()
+    accounts[0].transfer(gov, 10 * 1e18)
+    yield accounts.at(gov, force=True)
+
+
+@pytest.fixture
+def user(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture
+def token(interface, zapper):
+    yield interface.ERC20(zapper.want())
