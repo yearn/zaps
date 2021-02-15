609f1aac7af6574d192e09e6e0a9658b5f4d1af2
steffel <2143646+steffenix@users.noreply.github.com>
2021-02-15 19:04:39 +0100

    feat: add y3crv zap

---
+++ tests/y3crv/conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from brownie import config
+from brownie import Contract
+
+
+@pytest.fixture
+def zapper(accounts, Zapy3CRV):
+    zapper = accounts[0].deploy(Zapy3CRV)
+    yield zapper
+
+
+@pytest.fixture
+def vault(zapper):
+    vault = Contract(zapper.yVault())
+    yield vault
+
+
+@pytest.fixture
+def gov(accounts, vault):
+    gov = vault.governance()
+    accounts[0].transfer(gov, 10 * 1e18)
+    yield accounts.at(gov, force=True)
+
+
+@pytest.fixture
+def user(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture
+def token(interface, zapper):
+    yield interface.ERC20(zapper.want())
