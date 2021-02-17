9e65dc4cbfc0dba1e9378d225440c34d692c251b
b0dhidharma <b0dhidharma@protonmail.com>
2021-02-17 14:51:57 -0300

    feat: implements subsidizer contract

---
+++ interfaces/IChiToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IChiToken is IERC20 {
+    function mint(uint256 value) external;
+
+    function computeAddress2(uint256 salt) external view returns (address);
+
+    function free(uint256 value) external returns (uint256);
+
+    function freeUpTo(uint256 value) external returns (uint256);
+
+    function freeFrom(address from, uint256 value) external returns (uint256);
+
+    function freeFromUpTo(address from, uint256 value)
+        external
+        returns (uint256);
+}
