a6312f9b07710e0c1a93f58dab1cd9d5f70402f7
dougstorm <dougstorm@users.noreply.github.com>
2021-02-19 12:11:15 -0600

    style: black

--- scripts/deploy_trusted_vault_swap.py
+++ scripts/deploy_trusted_vault_swap.py
@@ -1,6 +1,8 @@
 from brownie import TrustedVaultMigrator, accounts, network
 from brownie.network.account import PublicKeyAccount
+
 import click
+
 
 def main():
     print(f"You are using the '{network.show_active()}' network")
@@ -16,5 +18,5 @@ def main():
         PublicKeyAccount("v2.registry.ychad.eth").address,
         "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c",  # Mainnet Chi Token
         {"from": dev},
-        publish_source=publish_source
+        publish_source=publish_source,
     )
